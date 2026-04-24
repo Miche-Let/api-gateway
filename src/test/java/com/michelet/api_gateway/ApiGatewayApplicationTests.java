a3b690669b87f104c63c3c62583e5de63cd07b65
Sehi55 <sehi1028@naver.com>
2026-04-25 03:20:19 +0900

    test: add application-test.yml

--- src/test/java/com/michelet/api_gateway/ApiGatewayApplicationTests.java
+++ src/test/java/com/michelet/api_gateway/ApiGatewayApplicationTests.java
@@ -2,7 +2,9 @@ package com.michelet.api_gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class ApiGatewayApplicationTests {
 
