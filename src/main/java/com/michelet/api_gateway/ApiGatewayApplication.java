0433dcaa0ca8d0547ddc439138c73790904386a8
Sehi55 <sehi1028@naver.com>
2026-04-25 02:30:16 +0900

    fix: add .gitignore

--- src/main/java/com/michelet/api_gateway/ApiGatewayApplication.java
+++ src/main/java/com/michelet/api_gateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.miche_let.api_gateway;
+package com.michelet.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
